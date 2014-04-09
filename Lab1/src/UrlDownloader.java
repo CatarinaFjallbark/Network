b386a1fc2a425955c05f58afd7ef81ef301d4d61
CatarinaSorensen <catarinasorensen1991@gmail.com>
2014-04-09 14:25:39 +0200

    Reading a Web Page
    Did a Write a Java program that takes a web page address as input and
    download the corresponding page.

---
+++ Lab1/src/UrlDownloader.java
@@ -0,0 +1,80 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
+
+public class UrlDownloader {
+	static URL url;
+
+
+	public UrlDownloader(String text){
+		try {
+			url = new URL(text);
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public URL getURL(){
+		return url;
+	}
+
+
+	public static void main(String[] args) {
+		System.out.print("What webbsite do you want to read? (Do not forget http://)\n");
+		Scanner scan = new Scanner(System.in);
+		String webbsite = scan.next();
+
+		try {
+			
+			UrlDownloader mainurl = new UrlDownloader(webbsite);
+			
+			// get URL content
+			url = mainurl.getURL();
+			URLConnection conn = url.openConnection();
+
+			// open the stream and put it into BufferedReader
+			BufferedReader br = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+
+			String inputLine;
+
+			//save to this filename
+			String fileName = "/Users/catarina/Desktop/test.html";
+			File file = new File(fileName);
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			//use FileWriter to write file
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+
+			while ((inputLine = br.readLine()) != null) {
+				bw.write(inputLine);
+			}
+
+			bw.close();
+			br.close();
+
+			System.out.println("Done");
+
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
+
